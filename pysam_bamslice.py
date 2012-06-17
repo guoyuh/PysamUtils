fe8433f6075754a643490400f3b7f25038fd8302
indapa <indapa@gmail.com>
2012-06-17 16:15:40 -0400

    initial commit

---
+++ pysam_bamslice.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import sys
+import os
+import string
+import re
+import subprocess
+from optparse import OptionParser
+
+
+def yield_bedcoordinate(fh):
+    """" yield a tuple of (chr, start,end) from bed file """
+    for line  in fh:
+        if '@' in line: continue
+        fields=line.strip().split("\t")
+        (chr, start, end) = fields[0:3]
+        yield(chr,start, end )
+
+
+""" generate a bam file with  window size around bed coordinates for a given BAM file - useful for doing alignment gazing/ Pysam processing on subsets of BAM files around regions of interest"""
+
+
+
+def main():
+    usage = "usage: %prog [options] bamfile  \n generate a shell script that generates locus specific BAM file of a certain   window size around bed coordinates for a given orignal BAM file\n\n"
+    parser = OptionParser(usage)
+    parser.add_option("--bedfile", type="string", dest="bedfile", help="bedfile")
+    parser.add_option("--window", type="int", default=100, dest="window", help="window" )
+    parser.add_option("--ref", type="string", default="human_reference_v37.fa", dest="ref", help="name of reference assembly (fasta) file (.fa)")
+    parser.add_option("--bamprefix", type="string", default=None, dest="bamprefix", help="output prefix of bam file")
+
+    (options, args)=parser.parse_args()
+    if options.bamprefix == None:
+        sys.write("please provide a value to --bamprefix option!\n ")
+        sys.exit(1)
+
+
+    bamfile=args[0]
+
+    if os.path.isfile(bamfile) == False:
+       sys.stderr.write("bam file doesn't exists! " + bamfile +"\n")
+       sys.exit(1)
+
+    bamindexfile=bamfile+".bai"
+
+    if os.path.isfile(bamindexfile) == False:
+        sys.stderr.write("bam index file doesn't exist! did you run samtools/bamtools index?\n")
+        sys.exit(1)
+
+    if os.path.isfile(options.bedfile) == False:
+        sys.stderr.write("bed file doesn't exist! " + options.bedfile + "\n")
+        exit(1)
+
+    bam = pysam.Samfile( bamfile, "rb" )
+
+    bedfh=open(options.bedfile, 'r')
+
+    for coord_tuple in yield_bedcoordinate(bedfh):
+        (chr, start, end)=coord_tuple
+        regionstring=chr+":"+start+".."+end
+        bamfilename=".".join( [ options.prefix, regionstring, 'bam' ] )
+
+        for alignedread in bam.fetch( chr, int(start), int(end) ):
+            print alignedread
+
+
+
+if __name__ == "__main__":
+    main()
